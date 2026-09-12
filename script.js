443ca94280edf46bd7c6fb0da6f6bfa185245dbf
Fumei0704 <jimmy10510518@gmail.com>
2026-09-12 20:16:36 +0800

    Add event listener for button click alert

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('btn-click');
+    
+    button.addEventListener('click', () => {
+        alert(' 🎉 恭喜你！網站的 JavaScript 正常運作中！');
+    });
+});
